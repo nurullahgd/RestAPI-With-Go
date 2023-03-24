6d5006308b20cefd61e3338f35a9797af3042649
naxy24 <nurullah.gnd65@gmail.com>
2023-03-24 11:31:12 +0300

    added database and docker

---
+++ sql/fill_tables.sql
@@ -0,0 +1,10 @@
+insert into roles(role_name) values ('Teacher');
+insert into roles(role_name) values ('Teacher');
+
+
+insert into users(name,surname,username,email,roleid) VALUES
+('Nurullah','Gündoğdu','ngun','ngundogdu@gmail.com',1),
+('İrem','Çömlekoğlu','iirem','iirem@gmail.com',2),
+('selami','çakir','seak','selamiçakr@gmail.com',2),
+('Selim','Kir','slmkr','slm@gmail.com',2),
+('Eray','Özcan','erAYY','eray@gmail.com',2);
